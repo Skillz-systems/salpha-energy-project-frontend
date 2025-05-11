d817dc2fd9573718b18029f9bed6e8e463a81ac4
Brisstone <okolijohnson69@gmail.com>
2025-05-12 00:50:15 +0100

    resolve-oss

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
-import tailwindcss from "tailwindcss";
-import tsconfigPaths from "vite-tsconfig-paths";
-// https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        sourcemap: false,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom']
-                }
-            }
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react-swc");
+const { resolve } = require("path");
+const tailwindcss = require("tailwindcss");
+const tsconfigPaths = require("vite-tsconfig-paths").default;
+
+module.exports = defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"]
         }
-    },
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        watch: {
-            usePolling: true,
-            followSymlinks: false,
-        },
-    },
-    plugins: [react(), tsconfigPaths()],
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-        },
-    },
-    optimizeDeps: {
-        include: ["@axios", "@react-icons"],
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss()],
-        },
-    },
+      }
+    }
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    watch: {
+      usePolling: true,
+      followSymlinks: false
+    }
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: ["plankton-app-v6zgk.ondigitalocean.app"]
+  },
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src")
+    }
+  },
+  optimizeDeps: {
+    include: ["axios", "react-icons"]
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  }
 });
