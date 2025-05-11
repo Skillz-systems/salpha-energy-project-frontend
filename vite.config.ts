ec61f6d70ad10bdd7d6d5e191a869d13d83708d8
Brisstone <okolijohnson69@gmail.com>
2025-05-11 23:31:19 +0100

    resolve-oss

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,37 @@ import tailwindcss from "tailwindcss";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    lib: {
+      name: 'web-ui',
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'web-ui',
+      formats: ['es']
+    },
+    rollupOptions: {
+      maxParallelFileOps: 2,
+      cache: false,
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+        sourcemap: true,
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        inlineDynamicImports: false,
+        // sourcemapIgnoreList: (relativeSourcePath) => {
+        //   const normalizedPath = path.normalize(relativeSourcePath);
+        //   return normalizedPath.includes('node_modules');
+        // },
+      }
+    }
+  },
   server: {
     host: "0.0.0.0",
     port: 5173,
