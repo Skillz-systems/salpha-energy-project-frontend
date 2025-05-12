265a50575081659a256fbb2556fcaf6ee30c12af
Brisstone <okolijohnson69@gmail.com>
2025-05-12 01:39:13 +0100

    resolve-oss

--- vite.config.js
+++ vite.config.js
@@ -22,12 +22,12 @@ export default defineConfig({
       usePolling: true,
       followSymlinks: false,
     },
-    allowedHosts: ["https://salpha-energy-rwgip.ondigitalocean.app"],
+    allowedHosts: ["plankton-app-v6zgk.ondigitalocean.app", "salpha-energy-rwgip.ondigitalocean.app"],
   },
   preview: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: ["https://salpha-energy-rwgip.ondigitalocean.app"],
+    allowedHosts: ["plankton-app-v6zgk.ondigitalocean.app", "salpha-energy-rwgip.ondigitalocean.app"],
   },
   plugins: [react(), tsconfigPaths()],
   resolve: {
